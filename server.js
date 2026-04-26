b1b3e036402a48e1691db22f11f759d5b4845a24
Marxeeeee <marclapid2006@gmail.com>
2026-04-26 21:33:16 +0800

    fix: server port updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -55,6 +56,6 @@ app.get('*path', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000, '0.0.0.0', () => {
-  console.log('Server running on port 5000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
