ce3073263a79428ba539544d7be79439b0596e21
Marxeeeee <marclapid2006@gmail.com>
2026-04-26 19:51:04 +0800

    fix: added .jsx file extension on app.js imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Login from "./pages/authentication/Login";
-import Dashboard from "./pages/overview/Dashboard";
-import Inventory from "./pages/inventory/Inventory";
-import Products from "./pages/inventory/Products";
-import Order from './pages/orders/Order.jsx';
-import Brands from "./pages/brands/Brands";
-import Suppliers from "./pages/supplier/Supplier";
-import StockManagement from "./pages/inventory/StockManagement";
-import Reports from "./pages/reports/Reports";
-import Settings from "./pages/settings/Settings";
-import AccountManagement from "./components/features/settings/AccountManagement.jsx";
-import Security from "./components/features/settings/Security";
-import AccountDetails from "./components/features/settings/AccountDetails";
+import Login from "./pages/authentication/Login.jsx";
+import Dashboard from "./pages/overview/Dashboard.jsx";
+import Inventory from "./pages/inventory/Inventory.jsx";
+import Products from "./pages/inventory/Products.jsx";
+import Order from './pages/orders/Order.jsx.jsx';
+import Brands from "./pages/brands/Brands.jsx";
+import Suppliers from "./pages/supplier/Supplier.jsx";
+import StockManagement from "./pages/inventory/StockManagement.jsx";
+import Reports from "./pages/reports/Reports.jsx";
+import Settings from "./pages/settings/Settings.jsx";
+import AccountManagement from "./components/features/settings/AccountManagement.jsx.jsx";
+import Security from "./components/features/settings/Security.jsx";
+import AccountDetails from "./components/features/settings/AccountDetails.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
